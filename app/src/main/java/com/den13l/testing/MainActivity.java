d81945fa2d06ee2113e74d3c5bf6ebae734588c7
Erdeni Erdyneev <erdeni.bat@gmail.com>
2016-06-23 11:34:22 +0600

    Create simple application with email button

---
+++ app/src/main/java/com/den13l/testing/MainActivity.java
@@ -0,0 +1,29 @@
+package com.den13l.testing;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendEmail(View view) {
+        String address = "ivan.ivanov@gmail.com";
+        String subject = "Hello, Ivan!";
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, address);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
